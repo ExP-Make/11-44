72b68dd5144448a1027862932ca9efe88227bc0f
HyungJun Lee <mirae1jun@gmail.com>
2025-07-29 23:03:32 +0900

    조작 버튼에 Pressed 효과 적용
    버튼을 누를 때 버튼_클릭 이미지로 교체해 버튼 누르는 효과 적용

--- Assets/Scripts/UI/ControlUI.cs
+++ Assets/Scripts/UI/ControlUI.cs
@@ -3,56 +3,88 @@ using UnityEngine.UI;
 
 public class ControlUI : MonoBehaviour
 {
+    [Header("Buttons")]
     public Button leftButton;
     public Button rightButton;
-    //public Button jumpButton;
     public Button interactButton;
     public Button itemButton;
 
+    [Header("Button Sprites")]
+    public Sprite leftButtonNormal;
+    public Sprite leftButtonPressed;
+    public Sprite rightButtonNormal;
+    public Sprite rightButtonPressed;
+    public Sprite interactButtonNormal;
+    public Sprite interactButtonPressed;
+    public Sprite itemButtonNormal;
+    public Sprite itemButtonPressed;
+
+    private Image leftButtonImage;
+    private Image rightButtonImage;
+    private Image interactButtonImage;
+    private Image itemButtonImage;
+
     private bool isLeftPressed = false;
     private bool isRightPressed = false;
-    private InventoryUI inventoryUI; // 인벤토리 UI 참조
+    private InventoryUI inventoryUI;
+
+    private void Awake()
+    {
+        // Get Image components from buttons
+        leftButtonImage = leftButton.GetComponent<Image>();
+        rightButtonImage = rightButton.GetComponent<Image>();
+        interactButtonImage = interactButton.GetComponent<Image>();
+        itemButtonImage = itemButton.GetComponent<Image>();
+
+        // Remove existing listeners as we will handle them with EventTriggers
+        interactButton.onClick.RemoveAllListeners();
+        itemButton.onClick.RemoveAllListeners();
+    }
 
     private void Start()
     {
-        inventoryUI = FindFirstObjectByType<InventoryUI>();
+        inventoryUI = FindObjectOfType<InventoryUI>();
         if (inventoryUI == null)
         {
             Debug.LogError("InventoryUI not found in the scene!");
         }
-    }
 
-    private void Awake()
-    {
-        // leftButton.onClick.AddListener(() => InputManager.Instance.SetHorizontal(-1f));
-        // rightButton.onClick.AddListener(() => InputManager.Instance.SetHorizontal(1f));
-        //jumpButton.onClick.AddListener(InputManager.Instance.OnJumpButtonClicked);
-        interactButton.onClick.AddListener(() => InputManager.Instance.OnInteractButtonDown());
-        itemButton.onClick.AddListener(() => InputManager.Instance.OnItemButtonDown());
+        // Set initial button images
+        if (leftButtonImage != null) leftButtonImage.sprite = leftButtonNormal;
+        if (rightButtonImage != null) rightButtonImage.sprite = rightButtonNormal;
+        if (interactButtonImage != null) interactButtonImage.sprite = interactButtonNormal;
+        if (itemButtonImage != null) itemButtonImage.sprite = itemButtonNormal;
     }
 
     void Update()
     {
-        bool isUIOpen = DialogManager.Instance.IsDialogOpen() || (inventoryUI != null && inventoryUI.IsPanelActive());
+        bool isInventoryOpen = (inventoryUI != null && inventoryUI.IsPanelActive());
+        bool isDialogueOpen = DialogManager.Instance.IsDialogOpen();
+        bool isUIOpen = isDialogueOpen || isInventoryOpen;
 
-        // 아이템 버튼은 인벤토리가 열려있을 때도 보여야 하므로 따로 처리
-        itemButton.gameObject.SetActive(!DialogManager.Instance.IsDialogOpen());
+        // 아이템 버튼은 대화창이 열려있을 때만 비활성화
+        itemButton.gameObject.SetActive(!isDialogueOpen);
 
         // 나머지 버튼들은 UI가 열려있으면 비활성화
         leftButton.gameObject.SetActive(!isUIOpen);
         rightButton.gameObject.SetActive(!isUIOpen);
         interactButton.gameObject.SetActive(!isUIOpen);
 
-        // 버튼이 비활성화되면 입력을 중지
+        // 버튼이 비활성화되면 입력을 중지하고 스프라이트를 원래대로 되돌림
         if (!leftButton.gameObject.activeSelf)
         {
             isLeftPressed = false;
+            if (leftButtonImage.sprite != leftButtonNormal)
+                leftButtonImage.sprite = leftButtonNormal;
         }
         if (!rightButton.gameObject.activeSelf)
         {
             isRightPressed = false;
+            if (rightButtonImage.sprite != rightButtonNormal)
+                rightButtonImage.sprite = rightButtonNormal;
         }
-        // 수평 입력 처리
+
+        // 버튼이 눌리고 있는 상태에 따라 매 프레임 입력을 전달합니다.
         if (isLeftPressed)
         {
             InputManager.Instance.SetHorizontal(-1f);
@@ -63,27 +95,60 @@ public class ControlUI : MonoBehaviour
         }
         else
         {
+            // 아무 버튼도 누르지 않으면 수평 입력을 0으로 설정하여 멈춥니다.
             InputManager.Instance.SetHorizontal(0f);
         }
     }
 
+    // --- EventTrigger가 호출할 public 메소드들 ---
+
+    // Left Button
     public void OnLeftButtonDown()
     {
         isLeftPressed = true;
+        if (leftButtonImage != null) leftButtonImage.sprite = leftButtonPressed;
     }
 
     public void OnLeftButtonUp()
     {
         isLeftPressed = false;
+        if (leftButtonImage != null) leftButtonImage.sprite = leftButtonNormal;
     }
 
+    // Right Button
     public void OnRightButtonDown()
     {
         isRightPressed = true;
+        if (rightButtonImage != null) rightButtonImage.sprite = rightButtonPressed;
     }
 
     public void OnRightButtonUp()
     {
         isRightPressed = false;
+        if (rightButtonImage != null) rightButtonImage.sprite = rightButtonNormal;
+    }
+
+    // Interact Button
+    public void OnInteractButtonDown()
+    {
+        InputManager.Instance.OnInteractButtonDown();
+        if (interactButtonImage != null) interactButtonImage.sprite = interactButtonPressed;
+    }
+
+    public void OnInteractButtonUp()
+    {
+        if (interactButtonImage != null) interactButtonImage.sprite = interactButtonNormal;
+    }
+
+    // Item Button
+    public void OnItemButtonDown()
+    {
+        InputManager.Instance.OnItemButtonDown();
+        if (itemButtonImage != null) itemButtonImage.sprite = itemButtonPressed;
+    }
+
+    public void OnItemButtonUp()
+    {
+        if (itemButtonImage != null) itemButtonImage.sprite = itemButtonNormal;
     }
 }
